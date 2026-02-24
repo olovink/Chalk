dd5c7cdc4d345aca7e2b453c8fb846aebd45d296
olovink <egortunekov98@gmail.com>
2026-02-24 14:11:19 +0700

    add: PMFormatter

--- src/chalk/formatter/PMFormatter.php
+++ src/chalk/formatter/PMFormatter.php
@@ -5,9 +5,26 @@ declare(strict_types=1);
 namespace chalk\formatter;
 
 use chalk\LogMessage;
+use pocketmine\thread\Thread;
+use pocketmine\thread\Worker;
+use pmmp\thread\Thread as NativeThread;
 
 class PMFormatter extends ContextInterpolator{
 	protected function doFormat(LogMessage $message, string $finalMessage): string{
-		return strtoupper($finalMessage);
+		$timestamp = $message->getDateTime();
+		$timeFormatted = $timestamp->format('H:i:s.v');
+
+		$thread = NativeThread::getCurrentThread();
+		if ($thread === null) {
+			$threadName = "Server thread";
+		} elseif ($thread instanceof Thread || $thread instanceof Worker) {
+			$threadName = $thread->getThreadName() . " thread";
+		} else {
+			$threadName = (new \ReflectionClass($thread))->getShortName() . " thread";
+		}
+
+		$level = $message->getLevel()->name;
+
+		return sprintf('[%s] [%s/%s]: %s', $timeFormatted, $threadName, $level, $finalMessage);
 	}
 }
